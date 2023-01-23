83deeade6d3c13ed300450dbdcb442ed939ef9e2
Ngoc Hưng Lê <ngochungle1331@gmail.com>
2023-01-23 08:55:55 +0700

    finish chat screen

---
+++ lib/pages/auth/register_page.dart
@@ -0,0 +1,194 @@
+import 'package:chatapp/helper/helper_function.dart';
+import 'package:chatapp/pages/auth/login_page.dart';
+import 'package:chatapp/pages/home_page.dart';
+import 'package:chatapp/service/auth_service.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+import '../../widgets/widgets.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final formKey = GlobalKey<FormState>();
+  String email = "";
+  String password = "";
+  String fullName = "";
+  bool _isLoading = false;
+  AuthService authService = AuthService();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(
+                  color: Theme.of(context).primaryColor),
+            )
+          : SingleChildScrollView(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      const Text(
+                        "Groupie",
+                        style: TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text(
+                        "Create your account now to chat and explore",
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w400),
+                      ),
+                      Image.asset("assets/register.png"),
+                      TextFormField(
+                        decoration: textInputDecoration.copyWith(
+                            labelText: "Full Name",
+                            prefixIcon: Icon(
+                              Icons.person,
+                              color: Theme.of(context).primaryColor,
+                            )),
+                        onChanged: ((value) {
+                          setState(() {
+                            fullName = value;
+                          });
+                        }),
+                        // check the validation
+
+                        validator: (val) {
+                          if (val!.isNotEmpty) {
+                            return null;
+                          } else {
+                            return "Name cannot be empty";
+                          }
+                        },
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      TextFormField(
+                        decoration: textInputDecoration.copyWith(
+                            labelText: "Email",
+                            prefixIcon: Icon(
+                              Icons.email,
+                              color: Theme.of(context).primaryColor,
+                            )),
+                        onChanged: ((value) {
+                          setState(() {
+                            email = value;
+                          });
+                        }),
+                        // check the validation
+
+                        validator: (val) {
+                          return RegExp(
+                                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                  .hasMatch(val!)
+                              ? null
+                              : "Please enter a valid email";
+                        },
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: textInputDecoration.copyWith(
+                            labelText: "Password",
+                            prefixIcon: Icon(
+                              Icons.lock,
+                              color: Theme.of(context).primaryColor,
+                            )),
+                        onChanged: ((value) {
+                          password = value;
+                        }),
+                        validator: (val) {
+                          if (val!.length < 6) {
+                            return "Password must be at least 6 characters";
+                          } else {
+                            return null;
+                          }
+                        },
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context).primaryColor,
+                              elevation: 0,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30))),
+                          child: const Text(
+                            "Register",
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                          onPressed: () {
+                            register();
+                          },
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text.rich(TextSpan(
+                          children: <TextSpan>[
+                            TextSpan(
+                                text: " Login now",
+                                style: const TextStyle(
+                                    color: Colors.black,
+                                    decoration: TextDecoration.underline),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    nextScreen(context, const LoginPage());
+                                  })
+                          ],
+                          text: "Already have an account?",
+                          style: const TextStyle(
+                              color: Colors.black, fontSize: 14))),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+    );
+  }
+
+  register() async {
+    if (formKey.currentState!.validate()) {
+      setState(() {
+        _isLoading = true;
+      });
+      await authService
+          .registerUserWithEmailAndPassword(fullName, email, password)
+          .then((value) async {
+        if (value == true) {
+          // saving the shared preferences
+          await HelperFunction.saveUserLoggedInStatus(true);
+          await HelperFunction.saveUserEmailSF(email);
+          await HelperFunction.saveUserNameSF(fullName);
+          nextScreenReplace(context, const HomePage());
+        } else {
+          showSnackBar(context, Colors.red, value);
+          setState(() {
+            _isLoading = false;
+          });
+        }
+      });
+    }
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:199178224959:web:dde47eea4ec751d2477f8a";
+  static String apiKey = "AIzaSyCbBLmP36fvayThbh7kThJ0bGIaJ4B9I0k";
+  static String messagingSenderId = "199178224959";
+  static String projectId = "chatapp-3433d";
+  final primaryColor = const Color(0xFFee7b64);
+}
